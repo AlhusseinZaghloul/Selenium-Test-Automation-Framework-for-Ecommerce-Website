67242e2666c7e69f03b24332f5526655b9f712c7
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-14 01:25:33 +0200

    Add methods to open login page and retrieve error message in LoginPage class

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -2,6 +2,7 @@ package pages;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import utils.BrowserActions;
 import utils.ElementsActions;
 
 public class LoginPage {
@@ -14,7 +15,12 @@ public class LoginPage {
     private final By username= By.id("user-name");
     private final By password= By.id("password");
     private final By loginButton= By.id("login-button");
+    private final By errorMessage= By.xpath("//h3[@data-test='error']");
 
+    public LoginPage openLoginPage() {
+        BrowserActions.navigateToUrl(driver, "https://www.saucedemo.com/");
+        return this;
+    }
     public LoginPage enterUsername(String userName) {
         ElementsActions.sendData(driver, username, userName);
         return this;
@@ -27,6 +33,10 @@ public class LoginPage {
         ElementsActions.clicking(driver, loginButton);
         return this;
     }
+    public String getErrorMessage() {
+        return ElementsActions.getText(driver, errorMessage);
+    }
+
 
 
 }
