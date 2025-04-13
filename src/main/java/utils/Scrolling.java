fb69ca28d5018b337ded3dd24dcdda6cea6d1fda
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-14 00:17:40 +0200

    Add Scrolling utility class for scrolling to elements in the viewport

---
+++ src/main/java/utils/Scrolling.java
@@ -0,0 +1,16 @@
+package utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+
+public class Scrolling {
+    private Scrolling() {
+        // Prevent instantiation
+    }
+
+    public static void scrollToElement(WebDriver driver, By locator) {
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(locator));
+    }
+}
