2ed2fb33f147a3f69c1f479f9297789a8e30f4d0
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-14 01:17:09 +0200

    Refactor scrollToElement method to use centralized findElement from ElementsActions

--- src/main/java/utils/Scrolling.java
+++ src/main/java/utils/Scrolling.java
@@ -11,6 +11,7 @@ public class Scrolling {
 
     public static void scrollToElement(WebDriver driver, By locator) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("arguments[0].scrollIntoView(true);", driver.findElement(locator));
+        js.executeScript("arguments[0].scrollIntoView(true);",
+                ElementsActions.findElement(driver, locator));
     }
 }
