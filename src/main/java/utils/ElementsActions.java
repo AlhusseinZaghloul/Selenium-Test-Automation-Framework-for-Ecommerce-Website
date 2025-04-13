940cca8e76d23c1c223525f2a8536d7bb1c75530
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-14 01:16:59 +0200

    Refactor element interaction methods to use a centralized findElement method

--- src/main/java/utils/ElementsActions.java
+++ src/main/java/utils/ElementsActions.java
@@ -3,6 +3,7 @@ package utils;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 
 public class ElementsActions {
@@ -20,10 +21,9 @@ public class ElementsActions {
     public static void sendData(WebDriver driver, By locator, String data) {
         Waits.waitForElementVisible(driver, locator);
         Scrolling.scrollToElement(driver, locator);
-        driver.findElement(locator).sendKeys(data);
+        findElement(driver, locator).sendKeys(data);
         LogsUtils.info("Data entered: ", data, " in element: ", locator.toString());
     }
-
     /**
      * Clicks on a specified web element after ensuring it's clickable.
      *
@@ -34,7 +34,7 @@ public class ElementsActions {
     public static void clicking(WebDriver driver, By locator) {
         Waits.waitForElementClickable(driver, locator);
         Scrolling.scrollToElement(driver, locator);
-        driver.findElement(locator).click();
+        findElement(driver, locator).click();
         LogsUtils.info("Clicked on element: ", locator.toString());
     }
     /**
@@ -44,10 +44,11 @@ public class ElementsActions {
      * @param locator The By locator of the target element.
      * @return The text content of the specified element as a String.
      */
+    @Step("Getting text from element: {locator}")
     public static String getText(WebDriver driver, By locator) {
         Waits.waitForElementVisible(driver, locator);
         Scrolling.scrollToElement(driver, locator);
-        String text = driver.findElement(locator).getText();
+        String text = findElement(driver, locator).getText();
         LogsUtils.info("Getting text: ", text, " from element: ", locator.toString());
         return text;
     }
@@ -63,7 +64,19 @@ public class ElementsActions {
     public static String getAttributeFromElement(WebDriver driver, By locator, String attribute) {
         Waits.waitForElementVisible(driver, locator);
         LogsUtils.info("Getting attribute: ", attribute, " from element: ", locator.toString());
-        return driver.findElement(locator).getDomAttribute(attribute);
+        return findElement(driver, locator).getDomAttribute(attribute);
+    }
+    /**
+     * Finds a web element using the specified locator after ensuring it's visible.
+     *
+     * @param driver  The WebDriver instance.
+     * @param locator The By locator of the target element.
+     * @return The found WebElement.
+     */
+    @Step("Finding element: {locator}")
+    public static WebElement findElement(WebDriver driver, By locator) {
+        Waits.waitForElementVisible(driver, locator);
+        return driver.findElement(locator);
     }
 
 
