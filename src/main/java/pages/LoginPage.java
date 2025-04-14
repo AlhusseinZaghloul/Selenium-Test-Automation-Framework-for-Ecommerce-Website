d0d079321cd1d7899c7a3a8937af00d8e31fefa4
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-14 23:14:49 +0200

    Add Allure step annotations to LoginPage methods for enhanced reporting

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,6 @@
 package pages;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utils.BrowserActions;
@@ -17,22 +18,27 @@ public class LoginPage {
     private final By loginButton= By.id("login-button");
     private final By errorMessage= By.xpath("//h3[@data-test='error']");
 
+    @Step("Open login page")
     public LoginPage openLoginPage() {
         BrowserActions.navigateToUrl(driver, "https://www.saucedemo.com/");
         return this;
     }
+    @Step("Enter username: {userName}")
     public LoginPage enterUsername(String userName) {
         ElementsActions.sendData(driver, username, userName);
         return this;
     }
+    @Step("Enter password: {passWord}")
     public LoginPage enterPassword(String passWord) {
         ElementsActions.sendData(driver, password, passWord);
         return this;
     }
+    @Step("Click login button")
     public LoginPage clickLoginButton() {
         ElementsActions.clicking(driver, loginButton);
         return this;
     }
+    @Step("Get error message")
     public String getErrorMessage() {
         return ElementsActions.getText(driver, errorMessage);
     }
