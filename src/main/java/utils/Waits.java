29cc24e235a1776b4140718890d4a5f40ba8bda0
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-14 21:38:55 +0200

    Add logging for wait conditions in Waits class

--- src/main/java/utils/Waits.java
+++ src/main/java/utils/Waits.java
@@ -12,14 +12,13 @@ public class Waits {
 
     //wait for element to be present
     public static WebElement waitForElementPresent(WebDriver driver, By locator) {
-       // code to wait for element to be present
        LogsUtils.info("Waiting for element to be present: ", locator.toString());
        return  new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(driver1 -> driver1.findElement(locator) );
     }
+
     //wait for element to be visible
     public static WebElement waitForElementVisible(WebDriver driver, By locator) {
-        // code to wait for element to be visible
         LogsUtils.info("Waiting for element to be visible: ", locator.toString());
         return new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(driver1 ->
@@ -29,9 +28,9 @@ public class Waits {
                 }
                 );
     }
+    
     //wait for element to be clickable
     public static WebElement waitForElementClickable(WebDriver driver, By locator) {
-        // code to wait for element to be clickable
         LogsUtils.info("Waiting for element to be clickable: ", locator.toString());
         return new WebDriverWait(driver, Duration.ofSeconds(10))
                 .until(driver1 ->
