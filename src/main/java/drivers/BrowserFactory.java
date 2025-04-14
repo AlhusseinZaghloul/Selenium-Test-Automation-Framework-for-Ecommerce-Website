04e0dd6fbae9c4f6f7b197f05d2938a7ce3eec9c
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-14 21:45:27 +0200

    Refactor EdgeOptions initialization in BrowserFactory for improved readability

--- src/main/java/drivers/BrowserFactory.java
+++ src/main/java/drivers/BrowserFactory.java
@@ -49,21 +49,16 @@ public class BrowserFactory {
                 return new FirefoxDriver(firefoxOptions);
 
             default:
-            EdgeOptions edgeOptions = new EdgeOptions();
-            edgeOptions.addArguments("--start-maximized");
-            edgeOptions.addArguments("--disable-extensions");
-            edgeOptions.addArguments("--disable-infobars");
-            edgeOptions.addArguments("--disable-notifications");
-            edgeOptions.addArguments("--remote-allow-origins=*");
-            Map<String, Object> edgePrefs = Map.of(
-                    "profile.default_content_setting_values.notifications", 2,
-                    "credentials_enable_service", false,
-                    "profile.password_manager_enabled", false,
-                    "autofill.profile_enabled", false
-            );
-            edgeOptions.setExperimentalOption("prefs", edgePrefs);
-            edgeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
-            //edgeOptions.addArguments("--headless");
+                EdgeOptions edgeOptions = new EdgeOptions();
+                edgeOptions.addArguments("--start-maximized");
+                edgeOptions.addArguments("--disable-extensions");
+                edgeOptions.addArguments("--disable-infobars");
+                edgeOptions.addArguments("--disable-notifications");
+                edgeOptions.addArguments("--remote-allow-origins=*");
+                Map<String, Object> edgePrefs = Map.of("profile.default_content_setting_values.notifications", 2, "credentials_enable_service", false, "profile.password_manager_enabled", false, "autofill.profile_enabled", false);
+                edgeOptions.setExperimentalOption("prefs", edgePrefs);
+                edgeOptions.setPageLoadStrategy(PageLoadStrategy.NORMAL);
+                //edgeOptions.addArguments("--headless");
                 return new EdgeDriver(edgeOptions);
         }
     }
