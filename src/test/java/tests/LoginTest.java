69222e11eaacc4e45b30419f56330c193454224c
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-15 01:02:25 +0200

    Refactor LoginTest to use dynamic browser type and external test data

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,38 +1,51 @@
 package tests;
 
 import drivers.DriverManager;
+import listeners.TestNGListeners;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 import pages.LoginPage;
+import utils.JsonUtils;
+import utils.PropertiesUtils;
 
+import static utils.PropertiesUtils.getPropertyValue;
+
+@Listeners(TestNGListeners.class)
 public class LoginTest {
     WebDriver driver;
+    String browserName;
+    JsonUtils testData;
+
+    @BeforeClass
+    public void init() {
+        browserName = PropertiesUtils.getPropertyValue("browserType");
+        testData = new JsonUtils("testData");
+    }
 
     @BeforeMethod
     public void setUp() {
-        driver = DriverManager.createInstance("edge");
-        new LoginPage(driver).openLoginPage();
+        driver = DriverManager.createInstance(browserName);
+        new LoginPage(driver).openLoginPage(getPropertyValue("loginPageURL"));
     }
 
     @Test
     public void loginTest() {
         new LoginPage(driver)
-                .enterUsername("standard_user")
-                .enterPassword("secret_sauce")
+                .enterUsername(testData.getJsonData("valid-login-credentials.username"))
+                .enterPassword(testData.getJsonData("valid-login-credentials.password"))
                 .clickLoginButton();
 
-        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+        String expectedUrl = getPropertyValue("homePageURL");
         String actualUrl = driver.getCurrentUrl();
         Assert.assertEquals(expectedUrl, actualUrl, "URL does not match after login");
     }
+
     @Test
     public void loginWithInvalidCredentials() {
         new LoginPage(driver)
-                .enterUsername("invalid_user")
-                .enterPassword("invalid_password")
+                .enterUsername(testData.getJsonData("invalid-login-credentials.username"))
+                .enterPassword(testData.getJsonData("invalid-login-credentials.password"))
                 .clickLoginButton();
 
         String expectedErrorMessage = "Epic sadface: Username and password do not match any user in this service";
