07213f007d170ca8fe1bedb6eeb6439972dac180
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-16 00:17:59 +0200

    Update wait methods to use configurable explicit wait duration from properties file

--- src/main/java/utils/Waits.java
+++ src/main/java/utils/Waits.java
@@ -13,14 +13,14 @@ public class Waits {
     //wait for element to be present
     public static WebElement waitForElementPresent(WebDriver driver, By locator) {
        LogsUtils.info("Waiting for element to be present: ", locator.toString());
-       return  new WebDriverWait(driver, Duration.ofSeconds(10))
+       return  new WebDriverWait(driver, Duration.ofSeconds(Long.parseLong(PropertiesUtils.getPropertyValue("explicit.wait"))))
                 .until(driver1 -> driver1.findElement(locator) );
     }
 
     //wait for element to be visible
     public static WebElement waitForElementVisible(WebDriver driver, By locator) {
         LogsUtils.info("Waiting for element to be visible: ", locator.toString());
-        return new WebDriverWait(driver, Duration.ofSeconds(10))
+        return new WebDriverWait(driver, Duration.ofSeconds(Long.parseLong(PropertiesUtils.getPropertyValue("explicit.wait"))))
                 .until(driver1 ->
                 {
                     WebElement element = waitForElementPresent(driver, locator);
@@ -32,7 +32,7 @@ public class Waits {
     //wait for element to be clickable
     public static WebElement waitForElementClickable(WebDriver driver, By locator) {
         LogsUtils.info("Waiting for element to be clickable: ", locator.toString());
-        return new WebDriverWait(driver, Duration.ofSeconds(10))
+        return new WebDriverWait(driver, Duration.ofSeconds(Long.parseLong(PropertiesUtils.getPropertyValue("explicit.wait"))))
                 .until(driver1 ->
                 {
                     WebElement element = waitForElementVisible(driver, locator);
