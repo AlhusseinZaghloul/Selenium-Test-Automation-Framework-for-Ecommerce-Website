436568d94a4eeddd4f46b46bf61d8845815208c9
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-18 01:40:07 +0200

    Add TestHomePage class to implement product addition tests

---
+++ src/test/java/tests/TestHomePage.java
@@ -0,0 +1,58 @@
+package tests;
+
+import drivers.GUIDriver;
+import listeners.TestNGListeners;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.*;
+import pages.HomePage;
+import pages.LoginPage;
+import utils.CustomSoftAssertion;
+import utils.JsonUtils;
+import utils.PropertiesUtils;
+import org.testng.asserts.SoftAssert;
+
+import static utils.CustomSoftAssertion.softAssertion;
+import static utils.PropertiesUtils.getPropertyValue;
+
+@Listeners(TestNGListeners.class)
+public class TestHomePage {
+    WebDriver driver;
+    String browserName;
+    JsonUtils testData;
+
+    @BeforeClass
+    public void init() {
+        testData = new JsonUtils("testData");
+        browserName = PropertiesUtils.getPropertyValue("browserName");
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new GUIDriver(browserName).get();
+        new LoginPage(driver).openLoginPage(getPropertyValue("loginPageURL"))
+                .performLogin(testData.getJsonData("valid-login-credentials.username"),
+                        testData.getJsonData("valid-login-credentials.password"));
+    }
+
+    @Test(description = "Verify that the user can add a product to the cart")
+    public void addProductToCartTest() {
+        new HomePage(driver)
+                .addProductToCart(testData.getJsonData("valid-products.product-1.name"));
+        softAssertion.assertTrue(new HomePage(driver)
+                .getTextOfAddToCartButton(testData.getJsonData("valid-products.product-1.name"))
+                .contains("Remove"));
+
+        new HomePage(driver).addProductToCart(testData.getJsonData("valid-products.product-2.name"));
+        softAssertion.assertTrue(new HomePage(driver)
+                .getTextOfAddToCartButton(testData.getJsonData("valid-products.product-2.name"))
+                .contains("Remove"));
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        GUIDriver.quitDriver();
+        CustomSoftAssertion.customAssertAll();
+    }
+
+}
