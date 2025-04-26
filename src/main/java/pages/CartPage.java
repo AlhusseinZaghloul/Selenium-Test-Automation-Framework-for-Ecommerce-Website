6395a3a7730962b5d8e56de9424513ab589e8b5e
Alhussein Zaghloul <152105415+AlhusseinZaghloul@users.noreply.github.com>
2025-04-26 16:43:22 +0300

    Add CartPage and CartTest classes to implement cart functionality and validate product prices in cart page

---
+++ src/main/java/pages/CartPage.java
@@ -0,0 +1,39 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.ElementsActions;
+
+public class CartPage {
+    private WebDriver driver;
+    private final By productName= By.cssSelector(".inventory_item_name");
+    private final By productPrice= By.cssSelector(".inventory_item_price");
+    private final By checkoutButton= By.cssSelector(".btn_action.checkout_button");
+
+    // dynamic XPath template: given a product name, find its price in the same cart_item
+    private static final String PRICE_BY_NAME_XPATH =
+            "//div[@class='inventory_item_name' and text()=\"%s\"]" +
+                    "/ancestor::div[contains(@class,'cart_item')]" +
+                    "//div[@class='inventory_item_price']";
+
+    public CartPage(WebDriver driver) {
+        this.driver= driver;
+    }
+
+    public String getProductName() {
+        return ElementsActions.getText(driver, productName);
+    }
+    public String getProductPrice() {
+        // if we only ever have one product on the page
+        return ElementsActions.getText(driver, productPrice);
+    }
+    // grab price by visible product name
+    public String getProductPriceByName(String name) {
+        By priceLocator = By.xpath(String.format(PRICE_BY_NAME_XPATH, name));
+        return ElementsActions.getText(driver, priceLocator);
+    }
+    public CartPage clickOnCheckoutButton() {
+        ElementsActions.clicking(driver, checkoutButton);
+        return this;
+    }
+}
---
+++ src/test/java/tests/CartTest.java
@@ -0,0 +1,86 @@
+package tests;
+
+import drivers.GUIDriver;
+import listeners.TestNGListeners;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.*;
+import pages.CartPage;
+import pages.HomePage;
+import pages.LoginPage;
+import utils.CustomSoftAssertion;
+import utils.JsonUtils;
+import utils.PropertiesUtils;
+
+import static utils.CustomSoftAssertion.softAssertion;
+import static utils.PropertiesUtils.getPropertyValue;
+
+@Listeners(TestNGListeners.class)
+public class CartTest {
+
+    WebDriver driver;
+    String browserName;
+    JsonUtils testData;
+    // product names from JSON
+    String productName1, productName2, productName3, productName4;
+    // expected prices from JSON
+    String expectedPrice1, expectedPrice2, expectedPrice3, expectedPrice4;
+
+    @BeforeClass
+    public void init() {
+        testData = new JsonUtils("testData");
+        browserName = getPropertyValue("browserName");
+        // Load product names from JSON
+        productName1 = testData.getJsonData("valid-products.product-1.name");
+        productName2 = testData.getJsonData("valid-products.product-2.name");
+        productName3 = testData.getJsonData("valid-products.product-3.name");
+        productName4 = testData.getJsonData("valid-products.product-4.name");
+        // Load expected prices from JSON
+        expectedPrice1 = testData.getJsonData("valid-products.product-1.price");
+        expectedPrice2 = testData.getJsonData("valid-products.product-2.price");
+        expectedPrice3 = testData.getJsonData("valid-products.product-3.price");
+        expectedPrice4 = testData.getJsonData("valid-products.product-4.price");
+    }
+
+    @BeforeMethod
+    public void setUp() {
+        driver = new GUIDriver(browserName).get();
+        new LoginPage(driver).openLoginPage(getPropertyValue("loginPageURL"))
+                .performLogin(testData.getJsonData("valid-login-credentials.username"),
+                        testData.getJsonData("valid-login-credentials.password"));
+    }
+
+    @Test(description = "Verify that the user can add multiple products to the cart and check their prices")
+    public void addProductToCartTest() {
+        new HomePage(driver)
+                .addProductToCart(productName1)
+                .addProductToCart(productName2)
+                .addProductToCart(productName3)
+                .addProductToCart(productName4)
+                .clickOnCartIcon();
+
+        CartPage cart = new CartPage(driver);
+
+        // -- assert each price via the dynamic locator
+        String actualPrice1 = cart.getProductPriceByName(productName1);
+        softAssertion.assertEquals(actualPrice1, expectedPrice1,
+                "Price mismatch for " + productName1);
+
+        String actualPrice2 = cart.getProductPriceByName(productName2);
+        softAssertion.assertEquals(actualPrice2, expectedPrice2,
+                "Price mismatch for " + productName2);
+
+        String actualPrice3 = cart.getProductPriceByName(productName3);
+        softAssertion.assertEquals(actualPrice3, expectedPrice3,
+                "Price mismatch for " + productName3);
+
+        String actualPrice4 = cart.getProductPriceByName(productName4);
+        softAssertion.assertEquals(actualPrice4, expectedPrice4,
+                "Price mismatch for " + productName4);
+
+    }
+    @AfterMethod
+    public void tearDown() {
+        GUIDriver.quitDriver();
+        CustomSoftAssertion.customAssertAll();
+    }
+}
